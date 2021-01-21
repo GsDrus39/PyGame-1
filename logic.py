3944cce55fead11ffd193fa8af24d0e198274a68
Bermy <pablonepikasso8000@gmail.com>
2021-01-21 22:57:05 +0300

    добавлены минимальные пояснения к методам (Glepp)

--- logic.py
+++ logic.py
@@ -8,10 +8,13 @@ class AI:
         self.hero = hero
 
     def attack(self):
+        """проверяется доступность героя и либо бот идет в комнату к герою и прячется в укрытие,
+         либо шагает из-за него и стреляет"""
         res = check_obstacle(self.enemy.x, self.enemy.y, self.hero.x, self.hero.y)
         if not res:
             self.go_to_hero()
         print('piu-piu')  # TODO сделать поиск укрытия и наведение на ГГ
 
     def go_to_hero(self):
+        """поиск пути к герою и движение к нему"""
         pass  # TODO сделать движение в комнату героя
